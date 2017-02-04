dc2aea88b4fedc8e4b4136f6c859ca1198a38dd4
pierrebiver <piiero57@gmail.com>
2017-02-04 19:18:20 +0100

    initial commit"

---
+++ main/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+	"gopkg.in/mgo.v2"
+	"net/url"
+	"encoding/json"
+	"gopkg.in/mgo.v2/bson"
+	"fmt"
+	"github.com/straight-to-the-code-service/descriptor"
+)
+
+func findHandler(db *mgo.Database) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		values, _ := url.ParseQuery(r.URL.RawQuery)
+		results := find(db.C("descriptors"), values.Get("s"))
+		byteResult, _ := json.Marshal(results)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(byteResult)
+	})
+}
+
+func find(c *mgo.Collection, queryStr string) []descriptor.Descriptor {
+	results := make([]descriptor.Descriptor, 0)
+	query := bson.M{"tags": bson.M{"$regex": fmt.Sprint(".*",queryStr,".*")}}
+	err := c.Find(query).All(&results)
+	if err != nil {
+		panic(err)
+	}
+	return results
+}
+
+func main() {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	db := session.DB("straightothecode")
+	defer session.Close()
+
+	http.Handle("/find/", findHandler(db))
+	http.ListenAndServe(":8080", nil)
+}
