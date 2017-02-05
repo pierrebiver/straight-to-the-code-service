c2a6d0c36caa8a3d92fbc0a196c125ca345af43e
pierrebiver <piiero57@gmail.com>
2017-02-05 18:11:27 +0100

    addd alltags services

--- main/main.go
+++ main/main.go
@@ -23,8 +23,28 @@ func findHandler(db *mgo.Database) http.Handler {
 
 func find(c *mgo.Collection, queryStr string) []descriptor.Descriptor {
 	results := make([]descriptor.Descriptor, 0)
-	query := bson.M{"tags": bson.M{"$regex": fmt.Sprint(".*",queryStr,".*")}}
+	query := bson.M{"tags": bson.M{"$regex": fmt.Sprint(".*", queryStr, ".*")}}
 	err := c.Find(query).All(&results)
+	if err != nil {
+		panic(err)
+	}
+	return results
+}
+
+func allTagHandler(db *mgo.Database) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		results := allTags(db.C("descriptors"))
+		byteResult, _ := json.Marshal(results)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(byteResult)
+	})
+}
+
+func allTags(c *mgo.Collection) []string {
+	results := make([]string, 0)
+
+	err := c.Find(nil).Select(bson.M{"tags": 1}).Distinct("tags",&results)
 	if err != nil {
 		panic(err)
 	}
@@ -40,5 +60,6 @@ func main() {
 	defer session.Close()
 
 	http.Handle("/find/", findHandler(db))
+	http.Handle("/alltags/", allTagHandler(db))
 	http.ListenAndServe(":8080", nil)
 }
